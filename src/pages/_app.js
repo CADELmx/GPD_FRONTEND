c7ab2aa9085292e1c9a646db23ce4841265d0633
Diego López Ariza <diegoawesome2017@outlook.com>
2024-11-07 12:35:27 -0600

    Updated icon images for manifest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,13 +5,6 @@ import Notify from '@/toast'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider } from 'next-themes'
 
-export const metadata = {
-  title: 'Gestión de Plantillas Docentes',
-  description: 'Gestión de Plantillas Docentes - UTIM',
-  generator: 'Next.js',
-  manifest: '/manifest.json',
-}
-
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
