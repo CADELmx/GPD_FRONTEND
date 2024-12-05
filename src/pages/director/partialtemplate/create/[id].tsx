57ddd27ca5de0833c43d1d0b7ac9ce7917e64051
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2024-12-05 11:52:20 -0600

    Update [id].tsx
    added revalidate of 1 second to getStaticProps

--- src/pages/director/partialtemplate/create/[id].tsx
+++ src/pages/director/partialtemplate/create/[id].tsx
@@ -64,7 +64,7 @@ export const getStaticProps = async ({ params: { id } }: { params: { id: string 
             areas,
             template,
             groups
-        }
+        }, revalidate: 1
     }
 }
 
